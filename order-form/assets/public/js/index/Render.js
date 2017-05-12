c360a567554f15ae2aaf505502a899bdb73faee1
mounir hamza <imha@emakina.com>
2017-05-12 11:47:19 +0200

    add render strategy

--- order-form/assets/public/js/index/Render.js
+++ order-form/assets/public/js/index/Render.js
@@ -13,19 +13,24 @@ define([], function () {
   Render.prototype = {
 
     /**
-     *
+     * Attach event listeners for event bus
      */
     attachEventListeners: function attachEventListeners () {
       this.eventBus.on('card:detect', this.displayNameCard);
     },
 
     /**
-     *
+     * display the name of card
      * @param event
      * @param data
      */
     displayNameCard: function displayNameCard (event, data) {
-      this.$typeCard.html(data);
+      if (data.card) {
+        const style = data.validation ? `<span class="label label-success">Valid</span>` : `<span class="label label-danger">Not Valid</span>`;
+        this.$typeCard.html(`${data.name} ${style}`);
+      } else {
+        this.$typeCard.html(data.name);
+      }
     },
 
   };
