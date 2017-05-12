33ead28e0d55a5188742ade32f5c660ae10f111a
mounir hamza <imha@emakina.com>
2017-05-12 11:07:28 +0200

    change to key up

--- order-form/assets/public/js/index/main.js
+++ order-form/assets/public/js/index/main.js
@@ -1,10 +1,13 @@
 /**
  * Created by imha on 11/05/2017.
  */
-require(['./service/eventBus'], function (eventBus) {
+require(['./index/service/eventBus', './index/Cards', './index/Render'], function (eventBus, Cards, Render) {
 
-  $( "#card" ).change(function(event) {
-    console.log($(event.target).val());
+  const cards = new Cards(eventBus);
+  const render = new Render(eventBus,$);
+
+  $('#card').keyup(function (event) {
+    eventBus.trigger('card:detect', cards.typeCard($(event.target).val()));
   });
 
 });
